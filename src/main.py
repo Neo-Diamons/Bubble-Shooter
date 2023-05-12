a6c2494176fc1a3cf81dc92a4218ef8c28f1dd93
DiamonsGold <romain.hure@epitech.eu>
2023-05-12 21:19:37 +0200

    feat(main): display current color on the shooter and change it when shoot.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-import math
 
 import pygame as pg
 import sys
-
-import pygame.draw
+from random import randint
+import math
 
 from bubbles import Bubbles
 
 
 colors: tuple = ("red", "green", "blue", "yellow", "purple", "orange")
+delay: int = 100
 
 
 def main():
@@ -18,6 +18,9 @@ def main():
     screen = pg.display.set_mode((600, 480))
     clock = pg.time.Clock()
     bubbles = Bubbles(screen, colors)
+    current_color = colors[randint(0, len(colors) - 1)]
+    dt: float = 0
+    time_elapsed: float = delay
     run = True
 
     while run:
@@ -31,6 +34,14 @@ def main():
 
         bubbles.update()
 
+        if time_elapsed >= delay:
+            if pg.mouse.get_pressed()[0]:
+                time_elapsed = 0
+                current_color = colors[randint(0, len(colors) - 1)]
+        else:
+            time_elapsed += dt
+            print(time_elapsed)
+
         screen.fill((30, 30, 30))
         bubbles.draw(screen)
 
@@ -41,9 +52,10 @@ def main():
         delta = (math.cos(angle) * dist, math.sin(angle) * dist)
         pg.draw.line(screen, (62, 63, 75), p1, (p1[0] + delta[0], p1[1] + delta[1]), 10)
         pg.draw.circle(screen, (52, 53, 65), p1, 25)
+        pg.draw.circle(screen, current_color, p1, 15)
 
         pg.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60)
 
     pg.quit()
     sys.exit()
